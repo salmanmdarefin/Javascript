7bbd1f53747085ad8bf9c6bbd849651ef50d4170
Md.Salman Arefin <73155268+salmanmdarefin@users.noreply.github.com>
2023-09-07 13:01:15 +0600

    Day 2

---
+++ d1index.js
@@ -0,0 +1,6 @@
+        alert("Smoking is injurious to Health");
+        document.write("<h1> Hi Buddy </h1>");
+        document.write("<h2>I Love Frontere Techonologies </h2>");
+        document.write("I am Md. Salman Arefin. </br>");
+        document.write("Assistant Software Quality Assurance Engineer </br>");
+
---
+++ d1index2.js
@@ -0,0 +1,4 @@
+document.write("Fronture Techonologies Limited </br>");
+document.write("Contact - +8801601555458 </br>");
+document.write("Email - slamanmdarefin@gmail.com </br>");
+console.log("I love my bike");
