f965442d92ad2d898ccdb5231558e908ad5bfe85
salmanmdarefin <salmanmdarefin@gmail.com>
2023-09-16 18:17:48 +0600

    day 3 task

---
+++ d3index1.js
@@ -0,0 +1,11 @@
+//var num = 23.6753;
+
+/* num = parseFloat (num);
+
+num = toString(num);
+console.log(typeof(num));*/
+
+//console.log (num.toFixed(2));
+//console.log (num.toPrecision(5));
+console.log(Number(true));
+console.log(Number(false));
